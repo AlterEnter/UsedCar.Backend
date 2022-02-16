71a7a515e91abcdce8df1523fc3a2cf53183cee5
Shan Zhuang <shan.z@mazda.co.jp>
2022-02-16 21:09:24 +0900

    Program.csの修正

--- src/UsedCar.Backend.Presentations.Functions.Users/Program.cs
+++ src/UsedCar.Backend.Presentations.Functions.Users/Program.cs
@@ -1,7 +1,9 @@
-using Microsoft.Azure.Functions.Worker.Configuration;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using UsedCar.Backend.UseCases.Users;
 
 namespace UsedCar.Backend.Presentations.Functions.Users
 {
@@ -9,11 +11,39 @@ namespace UsedCar.Backend.Presentations.Functions.Users
     {
         public static void Main()
         {
-            var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults()
+#if DEBUG
+            HttpClient.DefaultProxy = new WebProxy();
+#endif
+            IHost? host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults(worker =>
+                {
+                    //worker.UseNewtonsoftJson();
+                    //worker.UseMiddleware<UnhandledExceptionMiddleware>();
+                   //worker.UseMiddleware<AuthenticationMiddleware>();
+
+                })
+                //.ConfigureOpenApi()
+                .ConfigureServices(s =>
+                {
+                    ServiceProvider provider = s.BuildServiceProvider();
+                    IConfiguration configuration = provider.GetRequiredService<IConfiguration>();
+
+                    s.AddTransient<IUserCreateUseCase, UserCreateUseCase>();
+
+
+//                    s.AddDbContext<MsprDBContext>(options =>
+//                    {
+//#if DEBUG
+//                    options.UseSqlServer("Data Source=localhost\\sqlexpress;Integrated Security=True;Initial Catalog=UsedCarDB");
+//#else
+//                    options.UseSqlServer(configuration.GetConnectionString("DBContext"));
+//#endif
+//                });
+                })
                 .Build();
 
             host.Run();
         }
     }
+
 }
