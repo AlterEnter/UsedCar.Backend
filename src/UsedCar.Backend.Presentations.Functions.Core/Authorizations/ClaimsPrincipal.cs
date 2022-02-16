ca24bca3cb090b6e01f8e1b4199bcd90356f9aab
Shan Zhuang <shan.z@mazda.co.jp>
2022-02-16 20:55:32 +0900

    functions Core の実装

---
+++ src/UsedCar.Backend.Presentations.Functions.Core/Authorizations/ClaimsPrincipal.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+using Microsoft.Azure.Functions.Worker;
+
+namespace UsedCar.Backend.Presentations.Functions.Core.Authorizations;
+
+public class ClaimsPrincipalResolver
+{
+    private readonly FunctionContext _context;
+
+    public ClaimsPrincipalResolver(FunctionContext context) => _context = context;
+
+    public ClaimsPrincipal? GetClaimsPrincipal() => _context.Features.Get<ClaimsPrincipal>();
+}
---
+++ src/UsedCar.Backend.Presentations.Functions.Core/Authorizations/TokenResolver.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+
+namespace UsedCar.Backend.Presentations.Functions.Core.Authorizations
+{
+    public class TokenResolver
+    {
+        private readonly ClaimsPrincipal _user;
+
+        public TokenResolver(ClaimsPrincipal user) => _user = user;
+
+        public string GetId() => _user.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? throw new InvalidOperationException("ClaimTypes.NameIdentifierが取得できません");
+
+        public string GetMailAddress() => _user.Claims.FirstOrDefault(c => c.Type == "emails")?.Value ?? throw new InvalidOperationException("ClaimsPrincipalからemailsが取得できません");
+
+        public string GetDisplayName() => _user.Claims.FirstOrDefault(c => c.Type == "name")?.Value ?? throw new InvalidOperationException("ClaimsPrincipalからnameが取得できません");
+    }
+}
