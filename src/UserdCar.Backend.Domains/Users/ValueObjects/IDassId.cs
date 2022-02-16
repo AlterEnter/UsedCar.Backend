97b34899f6ab069b7a371a2a475d2d9b7df1b470
Shan Zhuang <shan.z@mazda.co.jp>
2022-02-16 20:03:24 +0900

    Domain変更（IdassIdの追加）

---
+++ src/UserdCar.Backend.Domains/Users/ValueObjects/IDassId.cs
@@ -0,0 +1,16 @@
+﻿namespace UsedCar.Backend.Domains.Users.ValueObjects
+{
+    public record IDassId
+    {
+        public IDassId(string idassid)
+        {
+            if (string.IsNullOrEmpty(idassid))
+            {
+                throw new ArgumentNullException(nameof(idassid), "idassid can not be null.");
+            }
+
+            Value = idassid;
+        }
+        public string Value { get; }
+    }
+}
