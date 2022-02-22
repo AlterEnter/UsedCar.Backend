ef39bf31605b0658b7ccedff3f3aec476380c5ff
zhu.ju <zhu.ju@mazda.co.jp>
2022-02-22 15:13:23 +0900

    comment commit

--- src/UsedCar.Backend.Infrastructures.EntityFrameworkCore/Users/IdaasRepository.cs
+++ src/UsedCar.Backend.Infrastructures.EntityFrameworkCore/Users/IdaasRepository.cs
@@ -7,13 +7,25 @@ namespace UsedCar.Backend.Infrastructures.EntityFrameworkCore.Users
 {
     public class IdaasRepository : IIdaasRepository
     {
+        /// <summary>
+        /// DBコンテキスト
+        /// </summary>
         private readonly UsedCarDBContext _dBContext;
 
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="dBContext"></param>
         public IdaasRepository(UsedCarDBContext dBContext)
         {
             _dBContext = dBContext;
         }
 
+        /// <summary>
+        /// 新規作成
+        /// </summary>
+        /// <param name="idaasInfo"></param>
+        /// <returns></returns>
         public async Task CreateAsync(Domains.Users.AggregateRoots.IdaasInfo idaasInfo)
         {
             var idaasInfoEntity = idaasInfo.ToTable();
@@ -23,6 +35,12 @@ namespace UsedCar.Backend.Infrastructures.EntityFrameworkCore.Users
             await _dBContext.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// 削除
+        /// </summary>
+        /// <param name="idaasInfo"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public async Task DeleteAsync(Domains.Users.AggregateRoots.IdaasInfo idaasInfo)
         {
             var idaasInfoEntity = await _dBContext.IdaasInfos
@@ -33,6 +51,11 @@ namespace UsedCar.Backend.Infrastructures.EntityFrameworkCore.Users
             await _dBContext.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// ユーザー検索
+        /// </summary>
+        /// <param name="idaasId"></param>
+        /// <returns></returns>
         public async Task<Domains.Users.AggregateRoots.IdaasInfo?> FindAsync(IdaasId idaasId) => await _dBContext
             .IdaasInfos
             .Where(i => i.IdpUserId == idaasId.Value)
@@ -42,6 +65,12 @@ namespace UsedCar.Backend.Infrastructures.EntityFrameworkCore.Users
                 new MailAddress(i.MailAddress)))
             .FirstOrDefaultAsync();
 
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="idaasInfo"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public async Task UpdateAsync(Domains.Users.AggregateRoots.IdaasInfo idaasInfo)
         {
             var idaasInfoEntity = await _dBContext.IdaasInfos
