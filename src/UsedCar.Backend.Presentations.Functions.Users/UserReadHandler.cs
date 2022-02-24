f266d6ac96474eacb8214505dbc6b57f4fe497af
zhu.ju <zhu.ju@mazda.co.jp>
2022-02-24 18:14:40 +0900

    UserDeleteHandler commit

--- src/UsedCar.Backend.Presentations.Functions.Users/UserReadHandler.cs
+++ src/UsedCar.Backend.Presentations.Functions.Users/UserReadHandler.cs
@@ -4,29 +4,23 @@ using System;
 using System.Net;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using AutoMapper;
 using UsedCar.Backend.Presentations.Functions.Core.Authorizations;
-using UsedCar.Backend.Presentations.Functions.Users.Models;
 using UsedCar.Backend.UseCases.Exceptions;
 using UsedCar.Backend.UseCases.Users;
-using UsedCar.Backend.Presentations.Functions.Users.Validations;
 
 namespace UsedCar.Backend.Presentations.Functions.Users
 {
-    public class UserUpdateHandler
+    public class UserReadHandler
     {
-        private readonly IUserUpdateUseCase _userUpdateUseCase;
+        private readonly IUserReadUseCase _userReadUseCase;
 
-        private readonly IMapper _mapper;
-
-        public UserUpdateHandler(IUserUpdateUseCase userUpdateUseCase, IMapper mapper)
+        public UserReadHandler(IUserReadUseCase userReadUseCase)
         {
-            _userUpdateUseCase = userUpdateUseCase;
-            _mapper = mapper;
+            _userReadUseCase = userReadUseCase;
         }
 
-        [Function($"{nameof(Users)}.{nameof(UserUpdateHandler)}")]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "users")] HttpRequestData req, FunctionContext context)
+        [Function($"{nameof(Users)}.{nameof(UserReadHandler)}")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "users")] HttpRequestData req, FunctionContext context)
         {
             HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
             ClaimsPrincipal? claimsPrincipal = new ClaimsPrincipalResolver(context).GetClaimsPrincipal();
@@ -50,18 +44,8 @@ namespace UsedCar.Backend.Presentations.Functions.Users
 
             try
             {
-                var validateResult = await req.GetRequestBodyAsync<UserUpdateRequest>();
-                if (validateResult.IsValid is false)
-                {
-                    response.StatusCode = HttpStatusCode.BadRequest;
-                    return response;
-                }
-
-                var userUpdateRequest = validateResult.Value;
-
-                var userUpdateRequestUseCase = _mapper.Map<UseCases.Users.Models.UserUpdateRequest>(userUpdateRequest);
-                userUpdateRequestUseCase.IdaasId = iDassId;
-                await _userUpdateUseCase.ExecuteAsync(userUpdateRequestUseCase);
+                var userReadResponse = await _userReadUseCase.ExecuteAsync(iDassId);
+                await response.WriteAsJsonAsync(userReadResponse);
                 return response;
             }
             catch (IdaasNotFoundException)
